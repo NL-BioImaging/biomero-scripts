6e17924a28c1af7f5d7104be9267586a605bf974
Luik <t.t.luik@amsterdamumc.nl>
2026-03-26 13:34:10 +0100

    Bump to 2.4.0

--- __workflows/SLURM_CellPose_Segmentation.py
+++ __workflows/SLURM_CellPose_Segmentation.py
@@ -51,7 +51,7 @@ from biomero import SlurmClient, constants
 import logging
 
 # Version constant for easy version management
-VERSION = "2.3.0"
+VERSION = "2.4.0"
 
 logger = logging.getLogger(__name__)
 
--- _data/SLURM_Remote_Conversion.py
+++ _data/SLURM_Remote_Conversion.py
@@ -49,7 +49,7 @@ CONV_OPTIONS_SOURCE = ['zarr']
 CONV_OPTIONS_TARGET = ['tiff', 'zarr']
 
 # Version constant for easy version management
-VERSION = "2.3.0"
+VERSION = "2.4.0"
 
 
 def runScript():
